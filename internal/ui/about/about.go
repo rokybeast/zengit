876cffd3b92a30864ef8030b3f57d8e256d3aed2
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-06-18 17:43:32 +0530

    fix: fix version (forgot about about :P)

--- internal/ui/about/about.go
+++ internal/ui/about/about.go
@@ -47,7 +47,7 @@ func (m Model) View() string {
 	body := bodyStyle.Render(
 		"gitty is a feature-rich, aesthetically good looking and mainly, a minimal TUI tool.\n" +
 			"built with golang, and with 󰋑 for  \n\n" +
-			"version: 0.1.0 alpha\n" + // yes, static version for now..
+			"version: 0.3.0 alpha\n" + // yes, static version for now..
 			"github repository (want to contribute?): github.com/rokybeast/gitty",
 	)
 	hint := hintStyle.Render("press esc to go back")
